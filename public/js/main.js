8fcc1bc9fc1a2354d7993911b08a4a6c121c3221
Nawar <nawaryossef2@gmail.com>
2017-07-27 19:45:27 -0400

    code refactor. store player and computer choices

--- public/js/main.js
+++ public/js/main.js
@@ -20,105 +20,65 @@ let winCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
 
 class Game {
   
-  collectPlayerChoices() {
-    $(document).ready(function(){
-      $(".square").click(function(){ 
-        let mark = Number($(this).attr('id'));  // convert passed id attribute to from string to number
-        allPayerChoices.push(mark);
-      });
-    });
-  }
+  // collectPlayerChoices() {
+  //   $(document).ready(function(){
+  //     $(".square").click(function(){ 
+  //       let mark = Number($(this).attr('id'));  // convert passed id attribute to from string to number
+  //       allPayerChoices.push(mark);
+  //     });
+  //   });
+  // }
   
-  displayAll() {
-    let that = this
+  play() {
+    let that = this;
     $(document).ready(function(){
       $(".square").click(function(){
        
-        // print mark
-        $(this).text(mark1); 
+       let idVal = $(this).attr('id');
+        // print player choice
+        that.stylePlayerChoice(idVal);
+        that.PlayerChoice(idVal);
         // 
         
-        // store player mark into marked squares
-        let playerMark = Number($(this).attr('id'));
-         markedSquares.push(playerMark);
-         markedSquares = _.uniq(markedSquares);
+        // store player mark into markedSquares array
+        that.getMarkedSquares(idVal);
+        
          
          //get empty squares 
-         emptySquares = allSquares.filter(function(ele) {
-           return markedSquares.indexOf(ele) < 0;
-        });
-         emptySquares = _.uniq(emptySquares);
-      // 
-         
-        // store computer mark into marked squares
-        let pos = _.shuffle(emptySquares)[0];
-        let computerMark = pos;
-        markedSquares.push(computerMark);
-        markedSquares = _.uniq(markedSquares);
-        // 
-        
-        
-        // computer mark
-        $("#" + pos).text(mark2);
-        $("#" + pos).css("font-size", "105px");
-        // 
-        
-         
+        that.getEmptySquares();
+      
+        // print computer Move
+        that.ComputerChoice();
+
         // get player and computer choices
-        allComputerChoices.push(pos);
-        allPayerChoices.push(playerMark);
-        //
-        
+        that.collectPlayerChoices(idVal);
+        that.collectComputerChoices(idVal);
       });
     });
   }
   
-  playerChoice() {
+  PlayerChoice(val) {
     $(document).ready(function(){
-      
-    })
+       $("#" + val).text(mark1);
+    });
   }
 
-  stylePlayerChoice(){
+  stylePlayerChoice(val){
     $(document).ready(function(){
-      $(".square").click(function(){
-           $(this).css("font-size", "105px");
-           $(this).css("padding-left", "50px");
+      $("#" + val).css("font-size", "105px");
+      $("#" + val).css("padding-left", "50px");
       });
-    });
   }
   
-  getMarkedSquares() {
+  ComputerChoice() {
+    let that = this;
     $(document).ready(function(){
-      $(".square").click(function(){
-        let mark = Number($(this).attr('id'));
-        markedSquares.push(mark);
-        markedSquares = _.uniq(markedSquares);
-        // console.log(markedSquares)
-      });
+      let computerMark = _.shuffle(emptySquares)[0]; // .shuffle is a lodash method
+      that.getMarkedSquares(computerMark); // store computer's choice into markedSquares array. 
+      $("#" + computerMark).text(mark2);
+      that.computerStyleChoice(computerMark);
     });
   }
-  
-  getEmptySquares() {
-    $(document).ready(function(){
-      $(".square").click(function(){
-       emptySquares = allSquares.filter(function(ele) {
-         return markedSquares.indexOf(ele) < 0;
-       });
-      // console.log(emptySquares)
-      });
-    });
-  }
-  
-  collectComputerChoices() {
-    $(document).ready(function(){
-      $(".square").click(function(){
-          let mark = Number($(this).attr('id'));
-          allComputerChoices.push(mark);
-      });
-    });
-  }
-  
   
   computerStyleChoice(pos) {
     $(document).ready(function(){
@@ -126,16 +86,36 @@ class Game {
     });
   }
   
+  
+  getMarkedSquares(idVal) {
+    markedSquares.push(Number(idVal));
+    markedSquares = _.uniq(markedSquares); // .uniq is a lodash method
+  }
+  
+  getEmptySquares() {
+    emptySquares = allSquares.
+      filter(function(ele) {
+        return markedSquares.indexOf(ele) < 0;
+      });
+         emptySquares = _.uniq(emptySquares);
+  }
+  
+  collectPlayerChoices(val) {
+     allPayerChoices.push(Number(val));
+     allPayerChoices = _.uniq(allPayerChoices);
+  }
+  
+  
+  collectComputerChoices(val) {
+     allComputerChoices.push(Number(val));
+     allComputerChoices = _.uniq(allComputerChoices);
+  }
 }
   
 var ttt = new Game();
-  
-  // ttt.getEmptySquares();
-  // ttt.getMarkedSquares();
-  ttt.displayAll();
-  ttt.stylePlayerChoice();
-  // ttt.collectPlayerChoices();
-  // ttt.collectComputerChoices();
+
+ttt.play();
+
    
 
 
