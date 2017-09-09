8127a2f3884e57af3cab8e26a25fa3c0a4280d5a
Nawar <nawaryossef2@gmail.com>
2017-09-09 13:13:37 -0400

    track player and computer score. track tie score

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,27 @@
 /* global $ */
+
+// things to be done : 
+
+// fix game reset
+// computer Ai
+// timing
+// add sound
+// end game flash effect
+// refactor code
+
+
 "use strict";
 
 const _ = require('lodash');
+const $ = require("jquery");
 
 const mark1 = 'X';
 const mark2 = 'O';
+
+
+let tieScore = 0
+let playerScore = 0
+let computerScore = 0
 
 
 let allPayerChoices = [];
@@ -14,52 +31,27 @@ let markedSquares = [];
 let emptySquares = [];
 
 let allSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 let winCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                [1, 4, 7], [2, 5, 8], [3, 6, 9],
                [1, 5, 9], [3, 5, 7]];
 
 class Game {
   
-  // collectPlayerChoices() {
-  //   $(document).ready(function(){
-  //     $(".square").click(function(){ 
-  //       let mark = Number($(this).attr('id'));  // convert passed id attribute to from string to number
-  //       allPayerChoices.push(mark);
-  //     });
-  //   });
-  // }
-  
-  play() {
+  playerMove() {
     let that = this;
     $(document).ready(function(){
-      $(".square").click(function(){
-       
-       let idVal = $(this).attr('id');
-        // print player choice
-        that.stylePlayerChoice(idVal);
-        that.PlayerChoice(idVal);
-        // 
-        
-        // store player mark into markedSquares array
+      $(".square").click(function(event){
+        let idVal = $(this).attr('id');
         that.getMarkedSquares(idVal);
-        
-         
-         //get empty squares 
         that.getEmptySquares();
-      
-        // print computer Move
-        that.ComputerChoice();
-
-        // get player and computer choices
-        that.collectPlayerChoices(idVal);
-        that.collectComputerChoices(idVal);
+        $("#" + idVal).text(mark1)
+        that.stylePlayerChoice(idVal);
+        that.collectPlayerChoices(idVal); 
+        that.preventOverwriteChoice();
+        that.updatePlayerScore();
+        that.updateTieScore()
       });
-    });
-  }
-  
-  PlayerChoice(val) {
-    $(document).ready(function(){
-       $("#" + val).text(mark1);
     });
   }
 
@@ -70,13 +62,25 @@ class Game {
       });
   }
   
-  ComputerChoice() {
+  computerMove() {
     let that = this;
     $(document).ready(function(){
-      let computerMark = _.shuffle(emptySquares)[0]; // .shuffle is a lodash method
-      that.getMarkedSquares(computerMark); // store computer's choice into markedSquares array. 
+      $(".square").click(function(){
+        let computerMark = _.shuffle(emptySquares)[0]; // .shuffle is a lodash method
+        that.getMarkedSquares(computerMark); // store computer's choice into markedSquares array.
+        that.getEmptySquares();
+        that.collectComputerChoices(computerMark);
+        that.printComputerChoice(computerMark); // print computer choice
+        that.preventOverwriteChoice();
+        that.computerStyleChoice(computerMark);
+        that.updateComputerScore();
+      });
+    });
+  }
+  
+  printComputerChoice(computerMark) {
+    $(document).ready(function(){
       $("#" + computerMark).text(mark2);
-      that.computerStyleChoice(computerMark);
     });
   }
   
@@ -86,6 +90,13 @@ class Game {
     });
   }
   
+  preventOverwriteChoice(){
+    $(document).ready(function(){
+      markedSquares.forEach(function(ele) {
+        $("#" + ele).off("click")
+      });
+    })
+  }
   
   getMarkedSquares(idVal) {
     markedSquares.push(Number(idVal));
@@ -97,7 +108,7 @@ class Game {
       filter(function(ele) {
         return markedSquares.indexOf(ele) < 0;
       });
-         emptySquares = _.uniq(emptySquares);
+    emptySquares = _.uniq(emptySquares);
   }
   
   collectPlayerChoices(val) {
@@ -110,12 +121,102 @@ class Game {
      allComputerChoices.push(Number(val));
      allComputerChoices = _.uniq(allComputerChoices);
   }
+ 
+ 
+ // scores 
+  
+  updateTieScore() {
+    let that = this;
+    $(document).ready(function(){
+      // markedSquares array has a NaN value stored as the last computer input in each game. therefore, length comparison between markedSquares and allSquares arrays must be done without counting that 'NaN' value as an index (markedSquares.length - 1)
+      if ((markedSquares.length - 1 === allSquares.length) && (that.didPlayerWin() === false && that.didComputerWin() === false)) {
+        $(".tie-score").text(tieScore += 1);
+        return true;
+      }
+    });
+  }
+  
+  didComputerWin() {
+    let funcReturn = false
+    winCombinations.forEach(function(ele) {
+      allComputerChoices.length === _.uniq(allComputerChoices.concat(ele)).length ? funcReturn = true : null
+    })
+    return funcReturn;
+  }
+  
+  updateComputerScore(val) {
+    let that = this;
+    $(document).ready(function(){
+      that.didComputerWin() ? $(".computer-score").text(computerScore += 1) : null;
+    });
+  }
+  
+  didPlayerWin() {
+    let funcReturn = false
+    winCombinations.forEach(function(ele) {
+      allPayerChoices.length === _.uniq(allPayerChoices.concat(ele)).length ? funcReturn = true : null
+    });
+    return funcReturn;
+  }
+  
+  updatePlayerScore(val) {
+    let that = this;
+    $(document).ready(function(){
+      that.didPlayerWin() ? $(".player-score").text(playerScore += 1) : null;
+      that.resetPlayer();
+    });
+  }
+  
+  resetPlayer() {
+    allPayerChoices = [];
+  }
+  gameReset() {
+    let that = this;
+    $(document).ready(function(){
+      $(".square").click(function(){
+        if (that.updatePlayerScore() || that.updateComputerScore() || that.updateTieScore()) {
+          allPayerChoices = [];
+          allComputerChoices = [];
+          markedSquares = [];
+          emptySquares = []; 
+          that.boardEmpty();
+        };
+      });
+    })
+  }
+  
+  boardEmpty() {
+    $(document).ready(function(){
+      $(".square").each(function(){
+        $(this).empty();
+       
+      });
+    });
+  }
+  
+  // bb() {
+  //   let that = this
+  //   $(document).ready(function(){
+  //     allSquares.forEach(function(ele) {
+  //       $("#" + ele).on("click", that.playerMove())
+  //       // $("#" + ele).on("click", that.computerMove())
+  //     })
+  //   });
+  // }
+  
+  
+  
+  play() {
+    this.playerMove();
+    this.computerMove();
+    this.gameReset();
+  }
 }
   
 var ttt = new Game();
 
 ttt.play();
-
+ 
    
 
 
---
+++ public/js/stats.js
@@ -0,0 +1,9 @@
+/* global $ */
+"use strict";
+let obj = {
+  mat: function(){
+    console.log("text")
+  }
+}
+
+module.exports = obj
